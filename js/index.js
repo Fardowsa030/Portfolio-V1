2369ac37f2a3b51ff895b5e0c27ccb29e933bab9
Fardowsa030 <fardowsa.hussein@student.hu.nl>
2020-05-10 13:21:20 +0200

    navbar change

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+
+var x = document.getElementById("menu");
+var burger = document.getElementById("burger");
+
+burger.addEventListener("click",function(){
+
+  if (x.style.left ==="0px") {
+    x.style.left ="-100%";
+  } 
+  else  {
+    x.style.left = "0px";
+  }
+
+});
+
+x.addEventListener('click', handleMenuClick);
+
+    function handleMenuClick(event) {
+ 
+      if (event.target instanceof HTMLAnchorElement) {
+        x.style.left ="-100%";
+      }
+    }
+
+
+const scroll = new SmoothScroll('.menu a[href*="#"]', {
+
+    speed: 800
+   });
+
+
 // Get the modal
 var modal1 = document.getElementById("Modal1");
 var modal2 = document.getElementById("Modal2");
@@ -15,53 +46,41 @@ var span3 = document.getElementsByClassName("close")[2];
 
 // When the user clicks on the button, open the modal
 project1.onclick = function() {
-  modal1.style.display = "block";
+  modal1.style.height = "100%";
 }
 
 project2.onclick = function() {
-    modal2.style.display = "block";
+    modal2.style.height = "100%";
   }
 
 project3.onclick = function() {
-   modal3.style.display = "block";
+   modal3.style.height = "100%";
   }
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function() {
-  modal1.style.display = "none";
+  modal1.style.height = "0px";
 }
 
 span2.onclick = function() {
-    modal2.style.display = "none";
+    modal2.style.height = "0px";
   }
 span3.onclick = function() {
-    modal3.style.display = "none";
+    modal3.style.height = "0px";
   }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal1) {
-    modal1.style.display = "none";
+    modal1.style.height = "0px";
   }
   else if(event.target == modal2){
-    modal2.style.display = "none";
+    modal2.style.height = "0px";
   }
 
   else if(event.target == modal3){
-    modal3.style.display = "none";
+    modal3.style.height = "0px";
 
   }
   
 }
-
-var x = document.getElementById("menu");
-var burger = document.getElementById("burger");
-
-burger.addEventListener("click",function(){
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-
-});
